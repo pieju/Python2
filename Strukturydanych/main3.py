5e0ad3e1a20365ea645a5e22cddea1726ba30ea3
pieju <131374850+pieju@users.noreply.github.com>
2023-06-18 14:23:39 +0200

    Delete Strukturydanych directory

--- Strukturydanych/main3.py
+++
@@ -1,7 +0,0 @@
-#zadanie3
-mylist= ["burak","cukinia", "pomidor","cytryna", "ananas","papryka", "dynia"]
-print("Podaj litere")
-letter=input()
-for element in mylist:
-    if element.startswith(letter):
-        print(element)
